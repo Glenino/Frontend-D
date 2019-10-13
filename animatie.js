e4e5564236169b46ab22286aa57d2832c11e812a
Glenino <55541875+Glenino@users.noreply.github.com>
2019-10-13 17:05:56 +0200

    Add files via upload

--- animatie.js
+++ animatie.js
@@ -4,3 +4,12 @@
 
 /*eslint 'no-console': 0*/
 
+
+var button1 = document.querySelector("button");
+var element1 = document.querySelector(".js");
+
+button1.addEventListener("click", function() {
+  element1.classList.toggle("yay");
+});
+
+/* bron javascript/html/css code: https://css-tricks.com/video-screencasts/150-hey-designers-know-one-thing-javascript-recommend/ */
